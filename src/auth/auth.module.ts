4b164d266787cd4c1e78d3f520ad382a6480a820
ewenlarrieu <ewenlarrieu200515@gmail.com>
2026-06-29 11:56:10 +0200

    Second commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 import type { SignOptions } from "jsonwebtoken";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { JwtStrategy } from "./jwt.strategy";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 import { UsersModule } from "../users/users.module";
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -21,7 +25,7 @@ import { UsersModule } from "../users/users.module";
       }),
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule {}            
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "../users/users.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private users: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET!,
+    });
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    const user = await this.users.findById(payload.sub);
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
+}
